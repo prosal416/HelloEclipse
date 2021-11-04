f5b7aa7b509692ba4c4be926e3f6d202c696c0ae
Paul Rosal <prosal@unb.ca>
2021-11-04 16:04:15 -0300

    version1

--- HelloProject/src/HelloApp.java
+++ HelloProject/src/HelloApp.java
@@ -2,7 +2,8 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Paul Rosal");
+		System.out.println("Paul Rosal, Hello");
+		
 	}
 
 }
